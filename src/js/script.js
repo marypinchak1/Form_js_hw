183155498b9b8ee5caf9cc82d292ec34634af01d
Mariia Pinchak <marypinchak1@gmail.com>
2023-02-18 12:14:59 +0200

    add-changes

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   // Attach a click event handler to the photo and change__photo elements
   $('#photo, .change__photo').click(function () {
-
+  
     // Show the file manager dialog and wait for a file to be selected
     const input = $('<input type="file">');
     input.on('change', function () {
@@ -29,6 +29,8 @@ $(document).ready(function () {
 });
 
 function getUserData() {
+
+  // Get the form fields
   const FORM = document.querySelector("form");
   const FNAME = document.getElementById("fname");
   const LNAME = document.getElementById("lname");
@@ -36,14 +38,18 @@ function getUserData() {
   const LOCATION_DATA = document.getElementById("location");
   const LANGUAGES_DATA = document.getElementById("languages");
 
+  // Add an event listener to the form
   FORM.addEventListener("submit", (event) => {
+    // Prevent the default behavior of the form
     event.preventDefault();
+    // Get the values of the form fields
     const FIRST_NAME = FNAME.value;
     const LAST_NAME = LNAME.value;
     const ABOUT = ABOUT_ME.value;
     const LOC_DATA = LOCATION_DATA.value;
     const LANG_DATA = LANGUAGES_DATA.value;
 
+    // Create an object with the data
     const dataUser = {
       First_Name: FIRST_NAME,
       Last_Name: LAST_NAME,
@@ -53,15 +59,15 @@ function getUserData() {
     }
 
     // Get existing data from local storage or create an empty array
-    const existingData = JSON.parse(localStorage.getItem("userData")) || [];
+    let usersData = JSON.parse(localStorage.getItem("userData")) || [];
 
     // Add the new data to the existing array
-    existingData.push(dataUser);
+    usersData.push(dataUser);
 
     // Save the updated array to local storage
-    localStorage.setItem("userData", JSON.stringify(existingData));
+    localStorage.setItem("userData", JSON.stringify(usersData));
 
-    console.log(existingData);
+    console.log(usersData);
   });
 };
 getUserData();
